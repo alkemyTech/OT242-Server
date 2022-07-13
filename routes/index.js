3a7d6271bb30b475fbc45f210c4ac1e7fa7586c1
Tomas Aranda <tomasaranda2018@gmail.com>
2022-07-13 15:09:24 -0300

    add get request to route organizations

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/organizations/1/public', (req, res, next) => {
+
+  // Petition test
+  const campos = {
+    name: 'Tomás',
+    img: "img.png",
+    phone: '3612536483',
+    address: 'san fernando 3522',
+    welcomeText: 'welcome to our organization'
+  }
+
+  res.json(campos)
+
+})
+
 module.exports = router;
