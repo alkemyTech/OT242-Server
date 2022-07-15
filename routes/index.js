1893d0535886c18083cd3c0d7c66198f91216317
Tomas Aranda <tomasaranda2018@gmail.com>
2022-07-15 10:54:59 -0300

    refactor removing request to organization route

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-// Get controller function
-const {getTestimonials} = require('../controllers/indexControllers')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
-
-// Get the testimonials
-router.get('/organizations/1/public', getTestimonials)
-
 
 
 
