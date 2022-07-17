887987c45a8816fe2ade25f56dd824a0e5766ee6
Tomas Aranda <tomasaranda2018@gmail.com>
2022-07-17 02:43:19 -0300

    refactor(server): add the correct paths

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 require('dotenv').config()
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const authRouter = require('./routes/auth');
 const organizationRouter = require('./routes/organization');
 
 const app = express();
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/auth', authRouter);
 app.use('/organizations', organizationRouter);
 
 // catch 404 and forward to error handler
