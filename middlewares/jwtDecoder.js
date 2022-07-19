e77a13992cefc49d09aeaf5ab29383757c1a8526
manubautista <manubautista2009@gmail.com>
2022-07-19 16:35:19 -0300

    minor code formatting

--- middlewares/jwtDecoder.js
+++ middlewares/jwtDecoder.js
@@ -1,12 +1,14 @@
-function parseJwt (token) {
-    return JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString());
+function parseJwt(token) {
+  return JSON.parse(Buffer.from(token.split(".")[1], "base64").toString());
 }
 
-const jwtDecoder = (req, res, next) =>{
-    const token = parseJwt('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmaXJzdE5hbWUiOiJNYW51ZWwiLCJsYXN0TmFtZSI6IkJhdXRpc3RhIiwiZW1haWwiOiJtYW51YmF1dGlzdGEyMDA5QGdtYWlsLmNvbSIsImlhdCI6MTY1ODE5NzA5MCwiZXhwIjoxNjU4MTk3NjkwfQ.QVTV-bPTIdgv4F4gE8VlSpFRXyYSHJreWgEd2iuSfgM');
-    res.send(token)
-}
+const jwtDecoder = (req, res, next) => {
+  const token = parseJwt(
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmaXJzdE5hbWUiOiJNYW51ZWwiLCJsYXN0TmFtZSI6IkJhdXRpc3RhIiwiZW1haWwiOiJtYW51YmF1dGlzdGEyMDA5QGdtYWlsLmNvbSIsImlhdCI6MTY1ODE5NzA5MCwiZXhwIjoxNjU4MTk3NjkwfQ.QVTV-bPTIdgv4F4gE8VlSpFRXyYSHJreWgEd2iuSfgM"
+  );
+  res.send(token);
+};
 
 module.exports = {
-    jwtDecoder
-}
+  jwtDecoder,
+};
--- routes/profile.js
+++ routes/profile.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const router = express.Router()
-const {getJWT} = require('../middlewares/jwtGenerator')
-const { jwtDecoder } = require('../middlewares/jwtDecoder')
+const express = require("express");
+const router = express.Router();
+const { getJWT } = require("../middlewares/jwtGenerator");
+const { jwtDecoder } = require("../middlewares/jwtDecoder");
 
-router.get('/generate', getJWT)
-router.get('/me', jwtDecoder)
-module.exports = router
+router.get("/generate", getJWT);
+router.get("/me", jwtDecoder);
+module.exports = router;
