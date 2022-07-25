157a34a1ef00703c910b4aab942af58afa38f49e
manubautista <manubautista2009@gmail.com>
2022-07-24 22:55:16 -0300

    code optimized

--- controllers/entryControllers.js
+++ controllers/entryControllers.js
@@ -2,23 +2,21 @@ const { Entry } = require("../models");
 
 const updateEntry = async (req, res) => {
   try {
-    const exist = await Entry.findAll({ //busca el entry por id
+    await Entry.update(req.body, {
       where: { id: req.params.id },
     });
-    if (exist != "") { // si existe hace update con los datos del body
-      await Entry.update(req.body, { 
-        where: { id: req.params.id },
-      });
 
-      const updatedEntry = await Entry.findAll({ // busca el entry actualizado
-        where: { id: req.params.id },
-      }); // responde con el entry actualizado
-      res.status(200).json({ message: "OK novedad actualizada", novedad: updatedEntry[0] }); 
+    const updatedEntry = await Entry.findAll({ // busca el entry actualizado
+      where: { id: req.params.id },
+    });
+    if (updatedEntry != "") {
+      // Si el entry existe responde con el entry actualizado
+      res.status(200).json({ message: "OK novedad actualizada", novedad: updatedEntry[0] });
     } else {
       throw new Error(); // si el entry no existe levanta un error
     }
   } catch (err) {
-    res.status(404).json({ message: "ERROR novedad inexistente" }); 
+    res.status(404).json({ message: "ERROR novedad inexistente" });
   }
 };
 
