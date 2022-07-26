af5309316ea129ca88cba08605822082c30f3861
Rafael Rodriguez Viau <rodriguezviaurafael@gmail.com>
2022-07-26 19:42:08 -0300

    Setup Amazon S3 SDK

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const logger = require('morgan');
 const cors = require('cors')
 require('dotenv').config()
 
+const { UploadImg, upload, VerifyMulterError, deleteImg } = require('./s3Services/s3')
+
 const indexRouter = require('./routes/index');
 const authRouter = require('./routes/auth');
 const organizationRouter = require('./routes/organization');
@@ -14,6 +16,42 @@ const newsRouter = require('./routes/admin/news')
 
 const app = express();
 app.use(cors())
+
+// To test s3
+////////////////////////////////////////////////////////////////////////////////
+/*
+app.post("/s3/upload", upload.array("file"), async (req, res) => {
+  try {
+      const results = await UploadImg(req.files);
+      console.log(results);
+      return res.json({ status: "success" });
+  } catch (err) {
+      console.log(err);
+  }
+});
+app.delete("/s3/delete/:id", async (req, res) => {
+  try {
+      const results = await deleteImg(req);
+      console.log(results);
+      return res.json({ status: "success" });
+  } catch (err) {
+      console.log(err);
+  }
+});
+app.use((error, req, res, next) => {
+  let er = VerifyMulterError(error);
+  if (er != "") {
+      return res.status(400).json({
+          message: er,
+      });
+  }
+});
+
+https://ot242-server.s3.sa-east-1.amazonaws.com/uploads/
+app.listen(5000, () => { console.log('listening on http://localhost:5000'); });
+*/
+////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -32,12 +70,12 @@ app.use('/auth', profileRouter);
 app.use('/admin/news', newsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -47,4 +85,14 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+
+//////////////////////////
+
+
+
+
+
+
+
+/////////////////////////
 module.exports = app;
