813a59f21a0b2bdd1334458f444a8107e55ebf2c
Gianluca <gianpetrii@gmail.com>
2022-08-02 11:46:56 -0300

    Escribo correctamente path de testimonials

--- controllers/testimonialsControllers.js
+++ controllers/testimonialsControllers.js
@@ -1,14 +1,38 @@
-const { testimonials } = require("../models");
+const { Testimonials } = require("../models");
+
 
 const createTestimonial = async (req, res, next) => {
   const { name, image, content } = req.body;
 
   try {
-    testimonials.create({ name, image, content });
+    Testimonials.create({ name, image, content });
     return res.status(200).json({ message: "Testimonial added." });
+
   } catch (err) {
     next(err);
   }
 };
 
-module.exports = { createTestimonial };
+const updateTestimonial = async (req, res) => {
+  try {
+    await Testimonials.update(req.body, {
+      where: { id: req.params.id },
+    });
+
+    const updatedTestimonial = await Testimonials.findAll({ // intento traer el testimonial con ese id
+      where: { id: req.params.id },
+    });
+    if (updatedTestimonial != "") { // En el caso que el testi con ese id exista envio el testimonio updated
+      res.status(200).json({ message: "Testimonio actualizado", testimonio: updatedTestimonial[0] });
+    } else {
+      throw new Error(); // si testi con ese id no existe tiro error
+    }
+  } catch (err) {
+    res.status(404).json({ message: "No existe un testimonio con ese id" });
+  }
+};
+
+
+
+
+module.exports = { createTestimonial, updateTestimonial };
--- routes/testimonials.js
+++ routes/testimonials.js
@@ -1,9 +1,15 @@
 var express = require("express");
 var router = express.Router();
-const { createTestimonial } = require("../controllers/testimonialsControllers");
+const { createTestimonial, updateTestimonial } = require("../controllers/testimonialsControllers");
 const {testimonialsValidationRules, validate} = require ('../middlewares/validator')
 
 /* Create a testimonial */
 router.post("/", testimonialsValidationRules(), validate, createTestimonial);
+
+
+/* Update testimonial */
+router.put("/:id", 
+  updateTestimonial
+);
 
 module.exports = router;
